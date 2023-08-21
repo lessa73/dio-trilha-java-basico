2520d50acfad39a02b7e82a39aa6aa14bf9441a9
Bowie <alexandrelessa73@gmail.com>
2023-08-21 10:24:57 -0300

    Classse scanner introduzida, mensagens para o usuário criadas e retorno dos valores.

--- ContaBanco/src/contaTerminal.java
+++ ContaBanco/src/contaTerminal.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        /*
+         * int numero = 0;
+         * String agencia = "";
+         * String nome = "";
+         * double saldo = 0;
+         */
+
+        System.out.println("Digite o número da conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite o número da agência: ");
+        String agencia = scanner.next();
+        scanner.nextLine();
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", Obrigado por criar uma conta em nosso banco,");
+        System.out.println("sua agência é " + agencia + " sua conta é " + numero + " e seu saldo " + saldo
+                + ", já está disponível para saque");
+
+        scanner.close();
+
     }
 }
